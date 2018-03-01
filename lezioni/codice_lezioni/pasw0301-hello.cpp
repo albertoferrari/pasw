41f755421fb5cab410b0fc82e6d4a1d2c86af0d6
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-03-01 19:38:12 +0100

    Add files via upload

---
+++ lezioni/codice_lezioni/pasw0301-hello.cpp
@@ -0,0 +1,10 @@
+
+/*
+* First C++ program that says hello (hello.cpp)
+*/
+#include <iostream>			// Needed to perform IO operations
+using namespace std;
+int main() {
+	cout << "hello, world" << endl;
+	return 0;
+}
