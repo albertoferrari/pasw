794cd57dd8057c584fff6e203c6b9c6d21e57c3f
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-03-06 19:40:30 +0100

    Add files via upload

---
+++ lezioni/codice_lezioni/pasw03b05-memoryLeak.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <new>
+
+using namespace std;
+
+/* funzione chiamata se l'allocazione di memoria di new fallisce*/
+void fine_memoria() {
+	cout << " *** memoria terminata *** ";
+	exit(0);
+}
+
+int main() {
+	int *v; long n;
+	set_new_handler(fine_memoria);
+	cout << "verranno allocati n array di n elementi -> n = "; cin >> n;
+	for (long i = 0; i < n; i++) {
+		v = new int[n];		// allocazione n interi
+		delete [] v;		// deallocazione
+	}
+	return 0;
+}
