3c71839de87da807d78c13b46a30ba434733135e
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-03-06 14:24:08 +0100

    Add files via upload

---
+++ lezioni/codice_lezioni/pasw03b01-puntatori.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main() {
+	int a; 			// a is an integer
+	int *aPtr; 			// aPtr is a pointer to an integer
+	a = 7;
+	aPtr = &a; 			// aPtr set to address of a
+	cout << "The address of a is " << &a << "\nThe value of aPtr is " << aPtr;
+	cout << "\n\nThe value of a is " << a << "\nThe value of *aPtr is " << *aPtr;
+	cout << "\n\nShowing that * and & are inverses of "<< "each other.\n&*aPtr = "
+		 << &*aPtr << "\n*&aPtr = " << *&aPtr << endl;
+	return 0;
+}
+ 
