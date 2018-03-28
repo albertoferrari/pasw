22a4fa940e2a328438e4c0b5ff7e9cd650d251c4
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-03-28 19:08:59 +0200

    Add files via upload

---
+++ lezioni/codice_lezioni/paswxx-eccezioni01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+    int num, den;	// numeratore e denominatore
+    double val;		// valore della frazione
+    cout << "numeratore: ";
+    cin >> num;
+    cout << "denominatore: ";
+    cin >> den;
+    if (den < 0)
+    {
+        cout << "denominatore = 0" << endl;
+    }
+    else
+    {
+        val = num/static_cast<double>(den);
+        cout << "valore della frazione: " << val << endl;
+    }
+    cout << "fine programma\n";
+    return 0;
+} 
